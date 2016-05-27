f6ca60974766c19999f0cd547a71edc05036dc49
Hidekazu Soga <hidekazu.soga@boostermedia.com>
2016-05-27 15:08:34 +0200

    test change

--- event/Get_Pos.js
+++ event/Get_Pos.js
@@ -1,10 +1,10 @@
-// ====================================================================================================
+// =============================================================================
 //
 // Cloud Code for Get_Pos, write your code here to customise the GameSparks platform.
 //
 // For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
 //
-// ====================================================================================================
+// =============================================================================
 
 var POS = Spark.getPlayer().getScriptData("POSVAR");
 
